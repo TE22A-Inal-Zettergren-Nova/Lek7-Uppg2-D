8255406ba3193364aa4081bd0493b30d481f2c4d
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-10-18 10:11:12 +0200

    skapat scanner

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner tb = new Scanner(System.in);
+        System.out.println("Hur mycket är klockan?");
+        double tid = tb.nextDouble();
+
+        }
+    }
+}
